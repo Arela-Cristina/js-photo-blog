7deeef986b1c23269e745344084f617ae5b8be4e
Arela-Cristina <instagrandine@gmail.com>
2024-11-06 13:47:52 +0100

    Create a function to add the data dynamically. invoke it later in the call to http

--- main.js
+++ main.js
@@ -3,15 +3,6 @@
 const cardsContainer = document.getElementById('cardsContainer');
 console.log(cardsContainer)
 
-//elemento card DOM
-let card = `
-        <div class="card">
-            <img src="https://i.scdn.co/image/ab67616d0000b273146487c9b1fa54ac381eb579" alt="descrizione img">
-            <div class="caption">
-                some text
-            </div>
-        </div>`
-
 //chiamiamo axios 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6') //chiamata HTTP => API
 
@@ -21,6 +12,27 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6') //chiamata HTT
 
     .then(res => {
         console.log(res.data) //debug
-        mail = res.data //stampiamo le  mails
-        // createLi(mail, '', 'list') // invochiamo la funzione, passiamo le mail e il UL = 'list'
+        data = res.data;
+        //invocchiamo la funzione pasandoli come parametro la nostra data
+        myFunction(data)
+
     })
+
+
+//creiammo una funzione per appendere le immagini nel teplate literal
+
+function myFunction(data) { //parametro data
+    data.forEach(({ url, title }) => {  // destrutturazione forEach prende tutti i 6 oggetti della data, scegliamo noi quali di tutti ci interessano. url e title
+        //aggiungiamo template literal
+        const card = ` 
+            <div class="card">
+                <img src="${url}" alt="${title}">
+                <div class="caption">
+                    ${title}
+                </div>
+            </div>`;
+
+        // appendiamo card al container
+        cardsContainer.innerHTML += card;
+    });
+}
