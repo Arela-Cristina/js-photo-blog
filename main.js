bd7c260db1df705bd4524c8400f2f5e394c5b45d
Arela-Cristina <instagrandine@gmail.com>
2024-11-07 11:09:44 +0100

    changue function name

--- main.js
+++ main.js
@@ -14,19 +14,18 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6') //chiamata HTT
         console.log(res.data) //debug
         data = res.data;
         //invocchiamo la funzione pasandoli come parametro la nostra data
-        myFunction(data)
+        appendToTemplate(data)
 
     })
 
 
 //creiammo una funzione per appendere le immagini nel teplate literal
-
-function myFunction(data) { //parametro data
-    data.forEach(({ url, title }) => {  // destrutturazione forEach prende tutti i 6 oggetti della data, scegliamo noi quali di tutti ci interessano. url e title
+function appendToTemplate(data) { //parametro data
+    data.forEach(({ url, title, id }) => {  // destrutturazione forEach prende tutti i 6 oggetti della data, scegliamo noi quali di tutti ci interessano. url e title
         //aggiungiamo template literal
         const card = ` 
             <div class="card">
-                <img src="${url}" alt="${title}">
+                <img src="${url}" alt="${title}" id="${id}">
                 <div class="caption">
                     ${title}
                 </div>
